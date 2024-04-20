86d8fe0443164311b1162e5d395b56011e5c878b
Filip Jirsák <filip@jirsak.org>
2024-04-20 08:00:32 +0200

    Initial commit

---
+++ src/db.ts
@@ -0,0 +1,143 @@
+import { HTTPException } from 'https://deno.land/x/hono@v4.2.5/mod.ts'
+import {ulid} from "https://deno.land/std@0.223.0/ulid/mod.ts";
+
+type Item = {
+    id?: string,
+    product: string,
+    amount: number,
+    unit?: string,
+    done?: boolean,
+}
+
+type DayItems = {
+    day: string,
+    items: Item[]
+}
+
+enum Day {
+    Mon = "mon",
+    Tue = "tue",
+    Wed = "wed",
+    Thu = "thu",
+    Fri = "fri",
+    Sat = "sat",
+    Sun = "sun",
+}
+
+const dayOrder = [Day.Mon, Day.Tue, Day.Wed, Day.Thu, Day.Fri, Day.Sat, Day.Sun]
+
+const kv = await Deno.openKv();
+
+const validateDay = (day: string): void => {
+    if (dayOrder.indexOf(day) < 0) {
+        throw new HTTPException(404, {message: `Invalid day: ${day}.`})
+    }
+}
+
+const buildItem = (entry: Deno.KvEntry<Item>): Item => {
+    const id: string = entry.key[2] as string
+    return {id, ...entry.value}
+}
+
+export const db = {
+    add: async (user: string, day: string, item: Item) => {
+        validateDay(day)
+        const id = ulid()
+        const result = await kv.set([user, day, id], {done: false, ...item})
+        return {
+            id,
+            ...item
+        }
+    },
+
+    get: async (user: string, day: string, id: string): Promise<Item | null> => {
+        validateDay(day)
+        const item = await kv.get<Item>([user, day, id]);
+        return item.value
+    },
+
+    edit: async (user: string, day: string, id: string, item: Item): Promise<Item | null> => {
+        validateDay(day)
+        const {value: currentItem} = await kv.get<Item>([user, day, id]);
+        if (currentItem === null) {
+            throw new HTTPException(404, {message: `Item with ID ${id} not found.`})
+        }
+        delete item.id
+        const result = await kv.set([user, day, id], item)
+        return {
+            id,
+            ...item
+        }
+    },
+
+    patch: async (user: string, day: string, id: string, item: Item): Promise<Item | null> => {
+        validateDay(day)
+        const {value: currentItem} = await kv.get<Item>([user, day, id]);
+        if (currentItem === null) {
+            throw new HTTPException(404, {message: `Item with ID ${id} not found.`})
+        }
+        delete item.id
+        const result = await kv.set([user, day, id], {...currentItem, ...item})
+        return {
+            id,
+            ...currentItem,
+            ...item
+        }
+    },
+
+    delete: async (user: string, day: string, id: string): Promise<void> => {
+        validateDay(day)
+        await kv.delete([user, day, id]);
+    },
+
+    day: async (user: string, day: string): Promise<Item[]> => {
+        validateDay(day)
+        const items = kv.list<Item>({prefix: [user, day]});
+        const result: Item[] = []
+        for await (const entry of items) {
+            result.push(buildItem(entry));
+        }
+        return result
+    },
+
+    week: async (user: string): Promise<DayItems[]> => {
+        const items = kv.list<Item>({prefix: [user]});
+        const result: DayItems[] = [
+            {
+                day: Day.Mon,
+                items: []
+            },
+            {
+                day: Day.Tue,
+                items: []
+            },
+            {
+                day: Day.Wed,
+                items: []
+            },
+            {
+                day: Day.Thu,
+                items: []
+            },
+            {
+                day: Day.Fri,
+                items: []
+            },
+            {
+                day: Day.Sat,
+                items: []
+            },
+            {
+                day: Day.Sun,
+                items: []
+            },
+        ]
+        for await (const entry of items) {
+            const day: string = entry.key[1] as string
+            const dayIndex = dayOrder.indexOf(day)
+            result[dayIndex].items.push(buildItem(entry));
+        }
+        return result
+    },
+
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { Hono, HTTPException } from 'https://deno.land/x/hono@v4.2.5/mod.ts'
+import { cors } from 'https://deno.land/x/hono@v4.2.5/middleware/cors/index.ts'
+import { db } from "./db.ts"
+
+const auth = async (c, next) => {
+    const user = c.req.header("Authorization")
+    if (user === undefined) {
+        throw new HTTPException(401, {message: `Missing Authorization header.`})
+    }
+    c.set("user", user)
+    await next()
+}
+
+const api = new Hono()
+
+api.use("/*", cors())
+api.use("/*", auth)
+
+api.get("/", async (c) => c.json(await db.week(c.get("user"))))
+api.get("/:day", async (c) => c.json(await db.day(c.get("user"), c.req.param("day"))))
+api.post("/:day", async (c) => c.json(await db.add(c.get("user"), c.req.param("day"), await c.req.json()), 201))
+api.put("/:day/:id", async (c) => c.json(await db.edit(c.get("user"), c.req.param("day"), c.req.param("id"), await c.req.json())))
+api.patch("/:day/:id", async (c) => c.json(await db.patch(c.get("user"), c.req.param("day"), c.req.param("id"), await c.req.json())))
+api.delete("/:day/:id", async (c) => c.json(await db.delete(c.get("user"), c.req.param("day"), c.req.param("id")), 204))
+
+const app = new Hono()
+app.route("/api", api)
+
+Deno.serve(app.fetch)
