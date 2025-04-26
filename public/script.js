8dafcb5679cde5c49026dfb12bb20163f12abf21
Filip Jirsák <filip@jirsak.org>
2025-04-26 09:58:57 +0200

    feat: Výchozí zobrazení seznamu

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const fetchData = async () => {
+  const resp = await fetch("/api/");
+  if (!resp.ok) {
+    alert("Chyba: " + resp.statusText);
+    throw resp;
+  }
+
+  return await resp.json();
+};
+
+const renderItem = ({ id, product, amount, unit, done }) => {
+  const classState = done ? "product--done" : "product--todo";
+  return `<li class="product ${classState}" id="product-${id}">
+    <span class="product__name">${product}</span>
+    <span class="product__amount">${amount ?? ""} ${unit ?? ""}</span>
+  </li>`;
+};
+
+const dayNames = {
+  mon: "pondělí",
+  tue: "úterý",
+  wed: "středa",
+  thu: "čtvrtek",
+  fri: "pátek",
+  sat: "sobota",
+  sun: "neděle",
+};
+
+const renderDay = ({ day, items }) => {
+  return `<div class="day"><h2>${
+    dayNames[day]
+  }</h2><ul class="day__items">${items.map(renderItem).join("")}</ul></div>`;
+};
+
+const data = await fetchData();
+document.querySelector("#app").innerHTML = `
+    <h1>Nákupy</h1>
+    <div class="week">
+        ${data.map((dayItem) => renderDay(dayItem)).join("")}
+    </div>
+`;
